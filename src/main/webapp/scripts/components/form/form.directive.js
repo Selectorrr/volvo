e1df08a1ceacd28f6b7979a4ecd8225520982419
Stepan Litvinov <litvinovsd@gmail.com>
2015-03-22 11:57:55 +0300

    volvo-12 Форма. Директивы. Добавить в проект директивы формы.

--- src/main/webapp/scripts/components/form/form.directive.js
+++ src/main/webapp/scripts/components/form/form.directive.js
@@ -24,4 +24,116 @@ angular.module('volvoApp')
                 });
             }
         };
+    })
+    .directive('myCheckbox', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                type: '@',
+                label: '@',
+                model: '='
+            },
+            link: function (scope, element, attrs) {
+            },
+            template: '<div class="checkbox">' +
+            '<label><input type="checkbox"' +
+            'ng-model="model">' +
+            '{{label}}</label>' +
+            '</div>'
+        };
+    }).directive('myInput', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                type: '@',
+                label: '@',
+                model: '=',
+                required: '=',
+                validation: '=',
+                name: '@',
+                mask: '@',
+                placeholder: '@',
+                pattern: '@'
+            },
+            compile: function compile(tElement, tAttrs, transclude) {
+                var $input = $(tElement).find('input');
+                $input.attr('name', tAttrs.name);
+                $input.attr('id', tAttrs.name);
+                if (tAttrs.placeholder) {
+                    $input.attr('placeholder', tAttrs.placeholder);
+                }
+                if (tAttrs.pattern) {
+                    $input.attr('ng-pattern', tAttrs.pattern);
+                }
+            },
+            template: '<div class="form-group" ng-class="{\'has-error\':validation(name)}">' +
+            '<label>{{label}}</label>' +
+            '<input type="{{type}}" class="form-control" ng-model="model" ng-required=required ui-mask="{{mask}}">' +
+            '<span class="help-inline text-danger" ng-show="validation(name)">{{validation(name)}}</span>' +
+            '</div>'
+        };
+    }).directive('mySelect', function () {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                label: '@',
+                model: '=',
+                required: '=',
+                validation: '=',
+                name: '@'
+            },
+            compile: function compile(tElement, tAttrs, transclude) {
+                var $input = $(tElement).find('select');
+                $input.attr('name', tAttrs.name);
+                $input.attr('id', tAttrs.name);
+            },
+            controller: function ($scope, $element, $attrs) {
+                $scope.$watch('required', function (value) {
+                    if (value && value.length !== 0) {
+                        var v = (function (string) {
+                            return typeof string == 'string' ? string.toLowerCase() : string;
+                        })("" + value);
+                        value = !(v == 'f' || v == '0' || v == 'false' || v == 'no' || v == 'n' || v == '[]');
+                    } else {
+                        value = false;
+                    }
+                    $scope.requiredBoolean = value;
+                });
+            },
+            template: '<div class="form-group" ng-class="{\'has-error\':validation(name)}">'
+            + '<label>{{label}}</label>'
+            + '<select class="form-control" ng-model="model" ng-required=requiredBoolean ng-transclude>'
+            + '</select>'
+            + '<span class="help-inline text-danger" ng-show="validation(name)">{{validation(name)}}</span>'
+            + '</div>'
+        };
+    }).directive('myYesNo', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                label: '@',
+                model: '=',
+                required: '=',
+                validation: '=',
+                name: '@'
+            },
+            compile: function compile(tElement, tAttrs, transclude) {
+                var $input = $(tElement).find('input[type=hidden]');
+                $input.attr('name', tAttrs.name);
+                $input.attr('id', tAttrs.name);
+            },
+            template: '<div class="form-group" ng-class="{\'has-error\':validation(name)}"><label >{{label}}</label><br/>' +
+            '<input type="hidden"  ng-model="model" ng-required=required>' +
+            '<div class="btn-group yesNo" data-toggle="buttons">' +
+            '<label class="btn btn-default" ng-class="{\'active\': model === true}" ng-click="model = true">' +
+            '<input type="radio" name="options" id="yes" autocomplete="off" ng-checked="model === true"> Да' +
+            '</label>' +
+            '<label class="btn btn-default" ng-class="{\'active\': model === false}" ng-click="model = false">' +
+            '<input type="radio" name="options" id="no" autocomplete="off" ng-checked="model === false"> Нет' +
+            '</label>' +
+            '</div>' +
+            '<div class="help-inline text-danger" ng-show="validation(name)">{{validation(name)}}</div>' +
+            '</div>'
+        };
     });
