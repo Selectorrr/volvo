f918a42bd6e1eb265b8d2017e2a1950bd0cca4a1
avallander <avallander@gmail.com>
2015-03-25 01:12:54 +0300

    вкладка новые автомобили: вторая табличка: заполнение с сервера + пересчет значений

--- src/main/webapp/scripts/app/tabs/tabNewCars/tabNewCars.controller.js
+++ src/main/webapp/scripts/app/tabs/tabNewCars/tabNewCars.controller.js
@@ -18,22 +18,8 @@ angular.module('volvoApp')
                 return;
             }
             recalculateNewVolvoRetail();
-        }
-
-        function recalculateNewVolvoRetail () {
-            $scope.model.tabNewCars.newVolvoRetailC30 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailC30);
-            $scope.model.tabNewCars.newVolvoRetailS40 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailS40);
-            $scope.model.tabNewCars.newVolvoRetailV40 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailV40);
-            $scope.model.tabNewCars.newVolvoRetailS60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailS60);
-            $scope.model.tabNewCars.newVolvoRetailXc60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailXc60);
-            $scope.model.tabNewCars.newVolvoRetailC70 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailC70);
-            $scope.model.tabNewCars.newVolvoRetailXc70 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailXc70);
-            $scope.model.tabNewCars.newVolvoRetailS80 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailS80);
-            $scope.model.tabNewCars.newVolvoRetailXc90 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailXc90);
-            $scope.model.tabNewCars.newVolvoRetailV60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailV60);
-
-            recalculateNewVolvoRetailTotal();
-            $scope.model.tabNewCars.newVolvoRetailTotal = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailTotal);
+            recalculateNewVolvoFlit();
+            recalculateNewVolvoTotal();
         }
 
         function recalculateGrossProfitForData (data) {
@@ -49,6 +35,24 @@ angular.module('volvoApp')
                 return;
             }
             return parseInt(data.second) - parseInt(data.third);
+        }
+
+        <!-- recalculateNewVolvoRetail -->
+
+        function recalculateNewVolvoRetail () {
+            $scope.model.tabNewCars.newVolvoRetailC30 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailC30);
+            $scope.model.tabNewCars.newVolvoRetailS40 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailS40);
+            $scope.model.tabNewCars.newVolvoRetailV40 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailV40);
+            $scope.model.tabNewCars.newVolvoRetailS60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailS60);
+            $scope.model.tabNewCars.newVolvoRetailXc60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailXc60);
+            $scope.model.tabNewCars.newVolvoRetailC70 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailC70);
+            $scope.model.tabNewCars.newVolvoRetailXc70 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailXc70);
+            $scope.model.tabNewCars.newVolvoRetailS80 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailS80);
+            $scope.model.tabNewCars.newVolvoRetailXc90 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailXc90);
+            $scope.model.tabNewCars.newVolvoRetailV60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailV60);
+
+            recalculateNewVolvoRetailTotal();
+            $scope.model.tabNewCars.newVolvoRetailTotal = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoRetailTotal);
         }
 
         function recalculateNewVolvoRetailTotal () {
@@ -91,6 +95,91 @@ angular.module('volvoApp')
             }
             if ($scope.model.tabNewCars.newVolvoRetailV60[type]) {
                 result += parseInt($scope.model.tabNewCars.newVolvoRetailV60[type]);
+            }
+            return result;
+        }
+
+        <!-- recalculateNewVolvoFlit -->
+
+        function recalculateNewVolvoFlit () {
+            $scope.model.tabNewCars.newVolvoFlitC30 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitC30);
+            $scope.model.tabNewCars.newVolvoFlitS40 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitS40);
+            $scope.model.tabNewCars.newVolvoFlitV40 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitV40);
+            $scope.model.tabNewCars.newVolvoFlitS60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitS60);
+            $scope.model.tabNewCars.newVolvoFlitXc60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitXc60);
+            $scope.model.tabNewCars.newVolvoFlitC70 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitC70);
+            $scope.model.tabNewCars.newVolvoFlitXc70 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitXc70);
+            $scope.model.tabNewCars.newVolvoFlitS80 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitS80);
+            $scope.model.tabNewCars.newVolvoFlitXc90 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitXc90);
+            $scope.model.tabNewCars.newVolvoFlitV60 = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitV60);
+
+            recalculateNewVolvoFlitTotal();
+            $scope.model.tabNewCars.newVolvoFlitTotal = recalculateGrossProfitForData($scope.model.tabNewCars.newVolvoFlitTotal);
+        }
+
+        function recalculateNewVolvoFlitTotal () {
+            $scope.model.tabNewCars.newVolvoFlitTotal = {
+                first: recalculateNewVolvoFlitTotalByType('first'),
+                second: recalculateNewVolvoFlitTotalByType('second'),
+                third: recalculateNewVolvoFlitTotalByType('third'),
+                fourth: recalculateNewVolvoFlitTotalByType('fourth')
+            }
+        }
+
+        function recalculateNewVolvoFlitTotalByType (type) {
+            var result = 0;
+            if ($scope.model.tabNewCars.newVolvoFlitC30[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitC30[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitS40[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitS40[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitV40[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitV40[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitS60[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitS60[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitXc60[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitXc60[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitC70[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitC70[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitXc70[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitXc70[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitS80[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitS80[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitXc90[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitXc90[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitV60[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitV60[type]);
+            }
+            return result;
+        }
+
+        <!-- recalculateNewVolvoTotal -->
+
+        function recalculateNewVolvoTotal () {
+            if (!$scope.model.tabNewCars.newVolvoTotal) {
+                $scope.model.tabNewCars.newVolvoTotal = {};
+            }
+            $scope.model.tabNewCars.newVolvoTotal.first = recalculateNewVolvoTotalByType('first');
+            $scope.model.tabNewCars.newVolvoTotal.second = recalculateNewVolvoTotalByType('second');
+            $scope.model.tabNewCars.newVolvoTotal.third = recalculateNewVolvoTotalByType('third');
+            $scope.model.tabNewCars.newVolvoTotal.fourth = recalculateNewVolvoTotalByType('fourth');
+        }
+
+        function recalculateNewVolvoTotalByType (type) {
+            var result = 0;
+            if ($scope.model.tabNewCars.newVolvoRetailTotal[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoRetailTotal[type]);
+            }
+            if ($scope.model.tabNewCars.newVolvoFlitTotal[type]) {
+                result += parseInt($scope.model.tabNewCars.newVolvoFlitTotal[type]);
             }
             return result;
         }
