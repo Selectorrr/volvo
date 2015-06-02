cf19a0dd3cac6ac28ec66ae3facc1daeb3d1d899
Stepan Litvinov <litvinovsd@gmail.com>
2015-06-03 01:14:18 +0300

    исправил ссылку на главную странцу

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -5,7 +5,11 @@ angular.module('volvoApp')
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.isInRole = Principal.isInRole;
         $scope.$state = $state;
-
+        $scope.params = {
+            year: new Date().getFullYear(),
+            month: new Date().toLocaleString("en-us", {month: "long"}).toLowerCase(),
+            kind: 'fact'
+        };
         $scope.logout = function () {
             Auth.logout();
             $state.go('login');
